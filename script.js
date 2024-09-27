641d347a2fb6a608f879a70d057697e2ef1a32d2
Matthews8850 <matthews714@nycstudents.net>
2024-09-27 13:17:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Matthew Santiag"; // update the part BETWEEN the "quotes"
+let userName = "Matthew-S714"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
